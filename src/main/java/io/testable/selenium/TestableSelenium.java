1955a65a37008792e8ed0882904aed4ae3b71199
Avi Stramer <astramer@gmail.com>
2018-03-29 13:50:06 -0500

    initial commit

---
+++ src/main/java/io/testable/selenium/TestableSelenium.java
@@ -0,0 +1,57 @@
+package io.testable.selenium;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Main entry point for creating a selenium driver for use on the Testable platform as well as a set of useful
+ * utilities like taking screenshots.
+ */
+public class TestableSelenium {
+
+    public static final int SELENIUM_PORT = Integer.getInteger("TESTABLE_SELENIUM_PORT", -1);
+    public static final String OUTPUT_DIR = System.getProperty("TESTABLE_OUTPUT_DIR");
+    public static final String REGION_NAME = System.getProperty("TESTABLE_REGION_NAME");
+    public static final String REGIONAL_CLIENT_INDEX = System.getProperty("TESTABLE_REGIONAL_CLIENT_INDEX");
+    public static final String ITERATION = System.getProperty("TESTABLE_ITERATION");
+
+
+    public static WebDriver newWebDriver(Capabilities capabilities) {
+        try {
+            int port = SELENIUM_PORT > 0 ? SELENIUM_PORT : 4444;
+            return new RemoteWebDriver(new URL("http://localhost:" + port + "/wd/hub"), capabilities);
+        } catch (MalformedURLException e) {
+            throw new WebDriverException(e);
+        }
+    }
+
+    public static Path takeScreenshot(WebDriver driver, String name) {
+        try {
+            File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            if (OUTPUT_DIR != null) {
+                return Files.copy(screenshot.toPath(), Paths.get(OUTPUT_DIR, toName(name)));
+            } else {
+                return screenshot.toPath();
+            }
+        } catch (IOException e) {
+            throw new WebDriverException(e);
+        }
+    }
+
+    private static String toName(String name) {
+        if (REGION_NAME != null) {
+            return REGION_NAME + "-" + REGIONAL_CLIENT_INDEX + "-" + ITERATION + "-" + name;
+        } else {
+            return name;
+        }
+    }
+
+}
